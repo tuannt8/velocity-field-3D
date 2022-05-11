3a90b80c9022a6c379e00fec6bbc52a6d5af559a
tuannt8 <tuannt8@gmail.com>
2022-05-12 00:00:17 +0200

    CPU bench:

--- include/vf_cpu_bench.h
+++ include/vf_cpu_bench.h
@@ -4,41 +4,38 @@
 #include <vector>
 #include <chrono>
 #include "common/Eigen/Dense"
+#include <iostream>
 
-#define mat_size 10000
+#define mat_size 2000
 
 class CPU_benchmark{
-    inline int index(int i, int j){
-        return i * mat_size + j;
-    }
+
 public:
     double measure_compute_pre_sec(){
-        Eigen::Matrix<double, mat_size, mat_size> A, B, C;
+        Eigen::MatrixXd A(mat_size, mat_size), B(mat_size, mat_size), C(mat_size, mat_size);
         A.fill(1.);
         B.fill(1.);
         C.fill(0.);
 
         double total_time = 0;
 
-
-//        std::vector<double>  A(mat_size* mat_size, 1);
-//        std::vector<double>  B(mat_size* mat_size, 1);
-//        std::vector<double>  C(mat_size* mat_size, 0);
-
-//        // Matrix multiplication
-//        for(int i = 0; i < mat_size; i++){
-//            for(int j = 0; j < mat_size; j++){
-//                double c = 0;
-//                for(int k = 0; k < mat_size; k++){
-//                    c += A[index(i,k)] * B[index(k,j)];
-//                }
-//                C[index(i,j)] = c;
-//            }
-//        }
-        // Check C
-
-
-        // Block matrix multiplication
+		auto start_time = std::chrono::system_clock::now();
+		
+		int num_iter = 10;
+		for(int i = 0; i < num_iter; i++)
+		{
+			std::cout << "iter " << i << std::endl;
+			C = C + A*B;
+		}
+		
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+			std::chrono::system_clock::now() - start_time);
+		std::cout << "Time: " << elapsed.count() << " ms" << std::endl;
+		double total = std::pow(double(mat_size),  3)*num_iter ;
+		std::cout << "Num operation: " << total << std::endl;
+		std::cout << "Compute power: " << double(total) / elapsed.count() * 1000 << " per sec " << std::endl;
+		
+		std::cout << "Squared norm of result: " << C.squaredNorm() << std::endl;
     }
 };
 
